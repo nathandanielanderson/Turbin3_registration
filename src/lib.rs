fad9debf748dcc7ecaeec8268f44b2f4d64f4de7
Nathan Anderson <nathandanielanderson@gmail.com>
2024-10-10 22:11:32 -0500

    Added test_turbin3_prereq and successfully registered

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+mod programs;
 
 #[cfg(test)]
 mod tests {
@@ -8,7 +6,7 @@ mod tests {
 
     use solana_client::{rpc_client::RpcClient};
     use solana_program::{pubkey::Pubkey, system_instruction::transfer};
-    use solana_sdk::{message::Message, signature::{Keypair, Signer, read_keypair_file}, transaction::Transaction};
+    use solana_sdk::{message::Message, signature::{Keypair, Signer, read_keypair_file}, transaction::Transaction, system_program};
     use bs58;
     use std::io::{self, BufRead};
     use std::str::FromStr;
@@ -16,6 +14,58 @@ mod tests {
     use dotenv::dotenv; 
 
     #[test]
+    fn test_turbin3_prereq() {
+        use crate::programs::Turbin3_prereq::{Turbin3PrereqProgram, CompleteArgs, UpdateArgs};
+
+        // Load environment variables from '.env'
+        dotenv().ok();
+
+        // Get RPC_URL from .env
+        let rpc_url = env::var("RPC_URL").expect("RPC_URL not set in .env");
+
+        // Create a Solana devnet connection
+        let rpc_client = RpcClient::new(rpc_url);
+
+        // Let's define our accoutns
+        let signer = read_keypair_file("Turbin3-wallet.json").expect("Couldn't find wallet file");
+
+        // Create PDA for our prereq account
+        let prereq = Turbin3PrereqProgram::derive_program_address(
+            &[
+                b"prereq",
+                &signer.pubkey().to_bytes().as_ref()
+             ]
+        );
+
+        // Define our instruction data
+        let args = CompleteArgs{
+            github:b"nathandanielanderson".to_vec()
+        };
+
+        // Get recent blockhash
+        let blockhash = rpc_client
+        .get_latest_blockhash()
+        .expect("Failed to get recent blockhash");
+
+        // Invoke "complete" function
+        let transaction = Turbin3PrereqProgram::complete(
+            &[&signer.pubkey(), &prereq, &system_program::ID], // Instruction
+            &args,
+            Some(&signer.pubkey()),
+            &[&signer],
+            blockhash
+        );
+
+        // Send the transaction
+        let signature = rpc_client
+        .send_and_confirm_transaction(&transaction)
+        .expect("Failed to send transaction");
+
+         // Print our transaction out
+         println!("Success! Check out your TX here: https://explorer.solana.com/tx/{}/?cluster=devnet", signature);
+
+    }
+
     fn keygen() {
 
         // Create a new keypair
@@ -97,14 +147,14 @@ mod tests {
         // Get RPC_URL from .env
         let rpc_url = env::var("RPC_URL").expect("RPC_URL not set in .env");
 
-        // Import our keypair
-        let keypair = read_keypair_file("dev-wallet.json").expect("Couldn't find wallet file");
-
-        // Define our Turbin3 public key
-        let to_pubkey = Pubkey::from_str("B7XdPS3HfCFt5RS3RH1t8xeH6XsaFL8Hj8wdWhNSLkWe").unwrap();
-
         // Create a Solana devnet connection
         let rpc_client = RpcClient::new(rpc_url);
+
+        // Import our keypair
+        let keypair = read_keypair_file("dev-wallet.json").expect("Couldn't find wallet file");
+        
+        // Define our Turbin3 public key
+        let to_pubkey = Pubkey::from_str("B7XdPS3HfCFt5RS3RH1t8xeH6XsaFL8Hj8wdWhNSLkWe").unwrap();
 
         // Get recent blockhash
         let recent_blockhash = rpc_client.get_latest_blockhash().expect("Failed to get recent blockhash");
---
+++ src/programs/mod.rs
@@ -0,0 +1,6 @@
+// Programs
+pub mod Turbin3_prereq;
+
+pub struct CompleteArgs {
+    pub github: Vec<u8>,
+}
